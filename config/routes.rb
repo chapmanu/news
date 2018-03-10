ac783743144c71b15e65655f8af9bddc03bb51d8
Kayla Ziegler <kziegler@chapman.edu>
2018-03-09 16:34:43 -0800

    Basic routes, view and controller for admin dashboard and user table

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
 
-  # Home Page
+  ############################
+  # Home
+  ############################
   get '/', to: 'home#index'
   get 'home', to: 'home#index'
+
+  ############################
+  # Admin Routes
+  ############################
+  get 'admin', to: 'admin/users#index'
 
   # Root
   root 'home#index'
