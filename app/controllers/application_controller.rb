3b76a13a7613bf254843a17840d2dcc5e163ee4f
Kayla Ziegler <kziegler@chapman.edu>
2018-03-12 15:23:45 -0700

    Configured strong params for Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
+
+  # See https://github.com/plataformatec/devise#strong-parameters
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
+  end
 end
