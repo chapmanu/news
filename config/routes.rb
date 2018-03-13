7ce5c91738f0e223363f76f6acfac3a233193b5b
Kayla Ziegler <kziegler@chapman.edu>
2018-03-12 17:45:25 -0700

    Removing admin_ prefix from user route names

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,10 @@ Rails.application.routes.draw do
   ############################
   get 'admin', to: 'admin/users#index'
 
+  # Need to list these individually to remove admin_ prefix
+  get 'admin/users/:id',      to: 'admin/users#show', as: 'user'
+  get 'admin/users/:id/edit', to: 'admin/users#edit', as: 'edit_user'
+
   # Root
   root 'home#index'
 end
